4f1b2b5c447e1caa28c915bfc7061a25887c2401
jeongjihwan <jgh4529@naver.com>
2026-03-24 13:10:35 +0900

    fix(infra): replace embedded TURN with external coturn to fix malformed ICE URLs (S14P21A603-424)

--- infra/livekit/entrypoint.sh
+++ infra/livekit/entrypoint.sh
@@ -2,4 +2,4 @@
 sed -e "s|\$LIVEKIT_NODE_IP|${LIVEKIT_NODE_IP}|g" \
     -e "s|\$TURN_SECRET|${TURN_SECRET}|g" \
     /etc/livekit.yaml.tpl > /tmp/livekit.yaml
-exec livekit-server --config /tmp/livekit.yaml --node-ip "${LIVEKIT_NODE_IP}"
+exec /livekit-server --config /tmp/livekit.yaml --node-ip "${LIVEKIT_NODE_IP}"
